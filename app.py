f14eecfb3d0f134115c6e9b90e77e1a2fed29803
Fauzi Kurniawan <98021139+Reinzkarnize@users.noreply.github.com>
2023-06-11 11:07:07 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,259 @@
+from flask import Flask, render_template, redirect, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from cs50 import SQL
+from pytube import Playlist, YouTube
+import os
+
+from helpers import apology, login_required, lookup
+
+app = Flask(__name__)
+
+# # folder upload file
+# UPLOAD_FOLDER = 'assets/playlist'
+# ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024  # Set maximum upload size to 32 megabytes
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///mytube.db")
+
+@app.route("/", methods=["GET", "POST"])
+@login_required
+def index():
+    if request.method == "POST":
+        url = request.form.get("yt-play-url")
+        title = request.form.get("play-title")
+
+
+        # Checking if user provided the required input
+        if not url:
+            return apology("must provide playlist URL", 401)
+        if not title:
+            return apology("must provide playlist title", 401)
+        try:
+            image = request.files['upload_image']
+        except:
+            return apology("Upload file error", 401)
+
+
+        # Checking valid playlist URL
+        videos = Playlist(url)
+
+        if not videos:
+            return apology("please provide valid URL")
+
+        # Scrape all title from playlist videos
+        video_title = []
+        for link in videos:
+            video_title.append(YouTube(link).title)
+
+        # Write submitted form to playlists database
+        db.execute("INSERT INTO playlists (title, user_id) VALUES (?, ?)" , title, session['user_id'])
+
+        # Select the newest playlist_id created to update video url and image name
+        playlist_id = db.execute("SELECT playlist_id FROM playlists ORDER BY created_at DESC LIMIT 1;")[0]['playlist_id']
+
+        # insert playlist videos to database
+        for vlink, vtitle in zip(videos, video_title):
+
+            # Convert video URLs to /embed
+            vlink = vlink.replace('/watch?v=', '/embed/')
+            db.execute("INSERT INTO videos (url, video_title, playlist_id) VALUES (?, ?, ?)", vlink, vtitle, playlist_id)
+
+        # if user provide valid image
+        if image:
+
+            # save file as playlist_id name
+            file_path = f"static/playlist/{playlist_id}.jpg"
+            image.save(file_path)
+
+            # update image name to database
+            db.execute("UPDATE playlists SET image = ? WHERE playlist_id = ?", f'{playlist_id}.jpg', playlist_id)
+
+        return redirect("/")
+
+        # return render_template("test.html", url=url, title=title, videos=videos, video_title=video_title)
+    else:
+
+        playlists = db.execute("SELECT * FROM playlists WHERE user_id = ?", session['user_id'])
+
+        return render_template("index.html", playlists=playlists)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        userName = request.form.get("username")
+        userPassword = request.form.get("password")
+        userConfirmation = request.form.get("confirm_password")
+
+        # Ensure username was submitted &
+        if not userName:
+            return apology("must provide username", 400)
+
+        # Ensure the username already exists/not
+        elif db.execute("SELECT username FROM users WHERE username = ?", userName):
+            return apology("username already exist", 400)
+
+        # Ensure password was submitted
+        elif not userPassword:
+            return apology("must provide password", 400)
+
+        # # Ensure confrimation was submitted
+        elif not userConfirmation:
+            return apology("must provide confirmation password", 400)
+
+        # Ensure password and confirmation password match
+        elif userPassword != userConfirmation:
+            return apology("passwords do not match", 400)
+
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", userName, generate_password_hash(userPassword))
+
+        return redirect("/")
+
+    else:
+        return render_template("register.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/playlist")
+@login_required
+def playlist():
+    playlist_id = request.args.get('playlist_id')
+    playlist_id = int(playlist_id)
+
+    videos = db.execute("SELECT * FROM videos WHERE playlist_id = ?", playlist_id)
+
+    playlists = db.execute("SELECT * FROM playlists WHERE user_id = ?", session['user_id'])
+
+    return render_template("playlist.html", videos=videos, playlists=playlists)
+
+
+@app.route("/remove")
+@login_required
+def remove():
+    playlist_id = request.args.get('playlist_id')
+    playlist_id = int(playlist_id)
+
+    image = db.execute("SELECT image FROM playlists WHERE playlist_id = ?", playlist_id)[0]['image']
+    image_path = f"static/playlist/{image}"
+
+    if os.path.exists(image_path):
+        os.remove(image_path)
+
+    db.execute("DELETE FROM playlists WHERE playlist_id = ?", playlist_id)
+    db.execute("DELETE FROM videos WHERE playlist_id = ?", playlist_id)
+
+    return redirect("/")
+
+
+
+@app.route("/settings")
+@login_required
+def settings():
+    user_name = db.execute("SELECT username FROM users WHERE id = ?", session['user_id'])
+
+    playlists = db.execute("SELECT * FROM playlists WHERE user_id = ?", session['user_id'])
+
+    return render_template("settings.html", user_name=user_name, playlists=playlists)
+
+@app.route("/change_usrname", methods=["GET", "POST"])
+@login_required
+def change_usrname():
+    if request.method == "POST":
+        userName = request.form.get("new_username")
+
+        if not userName:
+            return apology("Must provide new username", 401)
+
+        db.execute("UPDATE users SET username = ? WHERE id = ?", userName, session["user_id"])
+
+        return redirect("/settings")
+
+
+@app.route("/change_password", methods=["GET", "POST"])
+def change_pwd():
+    if request.method == "POST":
+        new_pwd = request.form.get("new_pwd")
+
+        # Query database for username
+        rows = db.execute("SELECT hash FROM users WHERE id = ?", session["user_id"])
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("old_pwd")):
+            return apology("Invalid password")
+
+        # Ensure password was submitted
+        elif not new_pwd:
+            return apology("must provide new password", 400)
+
+        # Ensure confrimation was submitted
+        elif not request.form.get("confirmation"):
+            return apology("must provide confirmation password", 400)
+
+        # Ensure passwords was match
+        elif request.form.get("confirmation") != new_pwd:
+            return apology("Passwords do not match", 400)
+
+        db.execute("UPDATE users SET hash = ? WHERE id = ?", generate_password_hash(new_pwd), session["user_id"])
+
+        return redirect("/settings")
+
+
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ readme.md
@@ -0,0 +1,46 @@
+# MyTube
+
+## Demo
+You can watch a demo of the MyTube project on [YouTube](https://youtu.be/cl2S9rl2kGs).
+
+## Details
+### Description
+MyTube is a web application that allows users to save YouTube playlists and watch videos from their playlists. This README file provides an overview of the project and guides users on how to set up and use the application.
+
+### Features
+
+- User Registration: Users can create an account to access the features of the website.
+- Playlist Management: Users can create new playlists, view their existing playlists, and remove playlists.
+- YouTube Playlist Import: Users can import YouTube playlists by providing the playlist URL.
+- Video Playback: Users can watch videos from their playlists.
+- Account Settings: Users can change their username and password.
+
+### Technologies Used
+
+The MyTube web application is built using the following technologies:
+
+- Python: Programming language used for the backend development.
+- Flask: Web framework used for building the application.
+- SQLite: Database management system used to store user information, playlists, and videos.
+- HTML/CSS: Frontend languages used for designing and styling the web pages.
+- JavaScript: Programming language used for client-side interactions.
+- PyTube: Python library used for parsing YouTube URLs and retrieving video information.
+- Flask-Session: Flask extension used for managing user sessions.
+
+### Installation
+
+To run the MyTube web application on your local machine, follow these steps:
+
+1. Navigate to the project directory: `cd mytube`
+2. Install the required dependencies: `pip install -r requirements.txt`
+3. Run the application: `flask run`
+4. Open your web browser and visit `http://localhost:5000` to access the MyTube application.
+
+### Usage
+
+1. Register a new account or log in with your existing account.
+2. On the homepage, you can view your existing playlists and create new playlists.
+3. To import a YouTube playlist, provide the playlist URL and a title for the playlist. Click on the submit button.
+4. You can watch videos from your playlists by clicking on the playlist and navigating to the playlist page.
+5. In the account settings, you can change your username and password.
+6. Log out of your account by clicking on the "Logout" button.
